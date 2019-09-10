acef28b394c09bb92af6e373aca8d892925c83b1
Luciana Pereira <luci.lv14@gmail.com>
2019-09-10 08:48:01 -0300

    Atualização exercicios aula 2

---
+++ Aula2/exe1.js
@@ -0,0 +1,19 @@
+function ex1a () {
+const computador = parseFloat(2500.00);
+
+const  mouse = parseFloat(57.99);
+
+const total = computador + mouse;
+
+console.log("Valor total da compra R$", total);
+} ex1a ()
+
+function ex1b () {
+    const farenheit1 = 30;
+
+const celsius1=((farenheit1 - 32)* 5 / 9);
+console.log("O Celsius é:" + " " + celsius1 + "ºC" );
+
+const kelvin1= parseFloat((farenheit1 - 32)* 5/9 + 273,15);
+console.log("O Kelvin é:" + " " + kelvin1 + "ºK" );
+} ex1b ()
