cad6b4ede3ed27c8ef26a3ce0f7425bb0dc313fc
Luciana Pereira <luci.lv14@gmail.com>
2019-09-10 08:44:34 -0300

    "Atualização exercicio da Aula 2"

---
+++ Aula2/exe3.js
@@ -0,0 +1,14 @@
+function exe3a (){
+    const simEsta = "Sim";
+const naoEsta = ! simEsta;
+const chave = "Esta aberta"
+
+const estaDestrancada = (simEsta || naoEsta);
+prompt("A Casa está destrancada?" + " " + estaDestrancada)
+ prompt("O usuario consegue entrar na casa?");
+} exe3a ()
+
+//Crie um programa que pergunta para o usuário:
+ //Está chovendo?
+ //Tem guarda-chuva?
+ //Imprima a resposta da pergunta "O usuário vai se molhar?"
